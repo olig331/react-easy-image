3145c6cca161a904f6d6c284f30c19cf023a8fe1
olig331 <61230543+olig331@users.noreply.github.com>
2021-01-14 13:55:01 +0000

    animted compoenet working

--- easyImage/AnimatedSlides/AnimatedSlides.js
+++ easyImage/AnimatedSlides/AnimatedSlides.js
@@ -21,27 +21,52 @@ export const AnimatedSlides = ({ images, userConfig, children }) => {
       dotBgColor: userConfig.dotBgColor ?? "#777",
       maxShadowBlur: userConfig.maxShadowBlur ?? 54,
       shadowColor: userConfig.shadowColor ?? "rgba(0,0,0, 0.19)",
+      shadows: userConfig.shadows ?? true,
       widthNumForCalc: parseInt(userConfig.imgWidth.replace(/([a-z]|[A-Z]|[%])/g, "")),
       heightNumForCalc: parseInt(userConfig.imgHeight.replace(/([a-z]|[A-Z]|[%])/g, "")),
       rotateTime: userConfig.rotateTime ?? 350
    };
 
+   useEffect(() => {
+      console.log("this is indexes arr", indexes)
+   }, [indexes])
+
+
+
 
    // React State 
+   const [indexes, setindexes] = useState([2, 3, 4]);
+   const [currentNumber, setcurrentNumber] = useState(() => Math.round(images.length / 2))
    const [iteration, setiteration] = useState(0);
    const [animateRight, setanimateRight] = useState(0);
    const [animateLeft, setanimateLeft] = useState(0);
-   const [currentNum, setCurrentNum] = useState(Math.round(images.length / 2));
+   const [mainImgDimensions, setmainImgDimension] = useState(0);
+
+   useEffect(() => {
+      let mainImgWidth = Math.round((width / 100) * 44).toString() + "px";
+      setmainImgDimension(mainImgWidth)
+   }, []);
+
+   useEffect(() => {
+      let mainImgWidth = Math.round((width / 100) * 44).toString() + "px";
+      setmainImgDimension(mainImgWidth)
+   }, [width])
+
+
+   useEffect(() => {
+      console.log("running use effect for dfimensiosn")
+      console.log(mainImgDimensions);
+   }, [mainImgDimensions])
 
    // Styling for the Left side image
    const leftSide = {
       transition: `${applyConfig.rotateTime}ms ease-in-out`,
       position: "absolute",
       top: "50%",
-      left: width < 2415 ? "8%" : "16%",
-      transform: width < 2415 ? "translate(-8%, -50%) perspective(600px) rotateY(-10deg)" : "translate(-16%, -50%) perspective(600px) rotateY(-10deg)",
+      left: width < 2415 ? "8%" : "10%",
+      transform: width < 2415 ? "translate(-8%, -50%) perspective(600px) rotateY(-10deg)" : "translate(-10%, -50%) perspective(600px) rotateY(-10deg)",
       maxWidth: `calc(${applyConfig.imgWidth} / 2.5)`,
-      width: "20%",
+      width: `calc(${mainImgDimensions} / 2.5)`,
       height: `calc((${applyConfig.heightNumForCalc} / 100)*22)px`,
       maxHeight: `calc(${applyConfig.imgHeight} / 2)`,
       zIndex: "1",
@@ -55,8 +80,8 @@ export const AnimatedSlides = ({ images, userConfig, children }) => {
       left: "50%",
       top: "50%",
       transform: "translate(-50%, -50%) perspective(0px) rotateY(0deg)",
-      width: `${applyConfig.imgWidth}`,
-      maxWidth: "45%",
+      maxWidth: `${applyConfig.imgWidth}`,
+      width: "44%",
       height: `calc((${applyConfig.heightNumForCalc} / 100)*22)px`,
       maxHeight: `${applyConfig.imgHeight}`,
       zIndex: "25",
@@ -66,16 +91,15 @@ export const AnimatedSlides = ({ images, userConfig, children }) => {
    const rightSide = {
       transition: `${applyConfig.rotateTime}ms ease-in-out`,
       position: "absolute",
-      left: width < 2415 ? "92%" : "84%",
+      left: width < 2415 ? "92%" : "90%",
       top: "50%",
-      transform: width < 2415 ? "translate(-92%, -50%) perspective(600px) rotateY(10deg)" : "translate(-84%, -50%) perspective(600px) rotateY(10deg)",
+      transform: width < 2415 ? "translate(-92%, -50%) perspective(600px) rotateY(10deg)" : "translate(-90%, -50%) perspective(600px) rotateY(10deg)",
       maxWidth: `calc(${applyConfig.imgWidth} / 2.5)`,
-      width: "20%",
+      width: `calc(${mainImgDimensions} / 2.5)`,
       height: `calc((${applyConfig.heightNumForCalc} / 100)*22)px`,
       maxHeight: `calc(${applyConfig.imgHeight} / 2)`,
       zIndex: "1",
    }
-
 
    // Emmpty Array tp push dot span elements to;
    const dots = [];
@@ -84,12 +108,44 @@ export const AnimatedSlides = ({ images, userConfig, children }) => {
       dots.push(
          <span
             key={index}
-            onClick={() => { setCurrentNum(index) }}
+            onClick={() => { setcurrentNumber(index) }}
             className="dot"
-            style={index === currentNum ? { backgroundColor: `${applyConfig.dotHighlightColor}` } : { backgroundColor: `${applyConfig.dotBgColor}` }}>
+            style={index === currentNumber ? { backgroundColor: `${applyConfig.dotHighlightColor}` } : { backgroundColor: `${applyConfig.dotBgColor}` }}>
          </span>
       );
    };
+
+
+   const caroselLogic = {
+
+      offset_index: function (image_index, offset) {
+         console.log((image_index + offset) + images.length) % images.length
+         return ((image_index + offset) + images.length) % images.length
+      },
+
+      right_button_press: function () {
+         let arr = [...indexes];
+         console.log("right before", arr, iteration)
+         let numToPush = arr[0];
+         numToPush = this.offset_index(arr[arr.length - 1], 1)
+         arr.shift();
+         arr.push(numToPush)
+         console.log("right after", arr, iteration)
+         setindexes(arr);
+      },
+
+      left_button_press: function () {
+         let arr = [...indexes];
+         console.log("left before", arr, iteration)
+         let numtoShift = arr[2];
+         numtoShift = this.offset_index(arr[0], -1);
+         arr.pop()
+         arr.unshift(numtoShift)
+         console.log("left after", arr, iteration)
+         setindexes(arr)
+      }
+   }
+
 
    return (
       <div css={css`width: 100%; height: ${applyConfig.containerHeight};`}>
@@ -99,7 +155,14 @@ export const AnimatedSlides = ({ images, userConfig, children }) => {
 
             {/* Left Side Img */}
             <img
-               src={currentNum === 0 ? images[images.length - 1].img.default : images[currentNum - 1].img.default}
+               src={
+                  iteration == 0
+                     ? images[indexes[0]].img.default
+                     : iteration == 1
+                        ? images[indexes[1]].img.default
+                        : images[indexes[2]].img.default
+               }
+
                css={css`filter: drop-shadow(0 1px 2px ${applyConfig.shadowColor}) 
                            drop-shadow(0 2px ${applyConfig.maxShadowBlur / 16}px ${applyConfig.shadowColor}) 
                            drop-shadow(0 4px ${applyConfig.maxShadowBlur / 8}px ${applyConfig.shadowColor})
@@ -118,15 +181,21 @@ export const AnimatedSlides = ({ images, userConfig, children }) => {
 
             {/* Middle Image */}
             <img
-               src={images[currentNum].img.default}
+               src={
+                  iteration == 0
+                     ? images[indexes[1]].img.default
+                     : iteration === 1
+                        ? images[indexes[2]].img.default
+                        : images[indexes[0]].img.default
+               }
                onClick={() => setanimateRight(1)}
                ani={animateRight}
                css={css`filter: drop-shadow(0 1px 2px ${applyConfig.shadowColor}) 
-                            drop-shadow(0 2px ${applyConfig.maxShadowBlur / 16}px ${applyConfig.shadowColor}) 
-                            drop-shadow(0 4px ${applyConfig.maxShadowBlur / 8}px ${applyConfig.shadowColor})
-                            drop-shadow(0 8px ${applyConfig.maxShadowBlur / 4}px ${applyConfig.shadowColor})
-                            drop-shadow(0 16px ${applyConfig.maxShadowBlur / 2}px ${applyConfig.shadowColor})
-                            drop-shadow(0 32px ${applyConfig.maxShadowBlur}px ${applyConfig.shadowColor})`}
+                           drop-shadow(0 2px ${applyConfig.maxShadowBlur / 16}px ${applyConfig.shadowColor}) 
+                           drop-shadow(0 4px ${applyConfig.maxShadowBlur / 8}px ${applyConfig.shadowColor})
+                           drop-shadow(0 8px ${applyConfig.maxShadowBlur / 4}px ${applyConfig.shadowColor})
+                           drop-shadow(0 16px ${applyConfig.maxShadowBlur / 2}px ${applyConfig.shadowColor})
+                           drop-shadow(0 32px ${applyConfig.maxShadowBlur}px ${applyConfig.shadowColor})`}
                style={
                   iteration === 0
                      ? middle
@@ -139,13 +208,20 @@ export const AnimatedSlides = ({ images, userConfig, children }) => {
 
             {/* Right Side Imgage */}
             <img
-               src={currentNum === images.length - 1 ? images[0].img.default : images[currentNum + 1].img.default}
+               src={
+                  iteration == 0
+                     ? images[indexes[2]].img.default
+                     : iteration == 1
+                        ? images[indexes[0]].img.default
+                        : images[indexes[1]].img.default
+               }
+
                css={css`filter: drop-shadow(0 1px 2px ${applyConfig.shadowColor}) 
-                            drop-shadow(0 2px ${applyConfig.maxShadowBlur / 16}px ${applyConfig.shadowColor}) 
-                            drop-shadow(0 4px ${applyConfig.maxShadowBlur / 8}px ${applyConfig.shadowColor})
-                            drop-shadow(0 8px ${applyConfig.maxShadowBlur / 4}px ${applyConfig.shadowColor})
-                            drop-shadow(0 16px ${applyConfig.maxShadowBlur / 2}px ${applyConfig.shadowColor})
-                            drop-shadow(0 32px ${applyConfig.maxShadowBlur}px ${applyConfig.shadowColor})`}
+                           drop-shadow(0 2px ${applyConfig.maxShadowBlur / 16}px ${applyConfig.shadowColor}) 
+                           drop-shadow(0 4px ${applyConfig.maxShadowBlur / 8}px ${applyConfig.shadowColor})
+                           drop-shadow(0 8px ${applyConfig.maxShadowBlur / 4}px ${applyConfig.shadowColor})
+                           drop-shadow(0 16px ${applyConfig.maxShadowBlur / 2}px ${applyConfig.shadowColor})
+                           drop-shadow(0 32px ${applyConfig.maxShadowBlur}px ${applyConfig.shadowColor})`}
                style={
                   iteration == 0
                      ? rightSide
@@ -155,7 +231,8 @@ export const AnimatedSlides = ({ images, userConfig, children }) => {
                            ? middle
                            : { color: "white" }}
             />
-            <div
+
+            {/* <div
                css={css`
                   position:absolute;
                   top: 90%;
@@ -163,11 +240,12 @@ export const AnimatedSlides = ({ images, userConfig, children }) => {
                   transform: translate(-50%, -90%);
                   `}>
                {dots}
-            </div>
+            </div> */}
 
          </div>
-         <button onClick={() => setiteration(prev => prev === 2 ? 0 : prev + 1)}>Rotate Right</button>
-         <button onClick={() => setiteration(prev => prev === 0 ? 2 : prev - 1)}>Rotate Left</button>
+         <button onClick={() => { caroselLogic.left_button_press(); setiteration(prev => prev === 2 ? 0 : prev + 1) }}>Rotate Left</button>
+         <button onClick={() => { caroselLogic.right_button_press(); setiteration(prev => prev === 0 ? 2 : prev - 1) }}>Rotate Right</button>
+         <button onClick={() => console.log(currentNumber)}>Current NUm</button>
       </div>
    )
 }
--- src/app.js
+++ src/app.js
@@ -3,104 +3,104 @@ import ReactDOM from 'react-dom';
 import { Stack } from '../easyImage/Stack/Stack';
 import { Tile } from '../easyImage/Tile/Tile';
 import { SimpleSlides } from '../easyImage/Slideshow/SimpleSlides';
-import './app.css';
 import card from './imgs/cards/10H.png';
 import { AnimatedSlides } from '../easyImage/AnimatedSlides/AnimatedSlides';
+import './app.css';
 
 
 const App = () => {
 
-    const slideArr = [
-        { img: require("./imgs/cards/2C.png"), cap: "2 of clubs" },
-        { img: require("./imgs/cards/5H.png"), cap: "5 of hearts" },
-        { img: require('./imgs/cards/3D.png'), cap: "3 of dimaonds" }
-    ];
 
-    const slideConfig = {
-        containerWidth: "350px",
-        containerHeight: "500px",
-        imgWidth: "250px",
-        imgHeight: "400px",
-        shadowColor: "rgba(0,0,0,0.09)",
-        allowDots: true
-    }
+   const slideArr = [
+      { img: require("./imgs/cards/2C.png"), cap: "2 of clubs" },
+      { img: require("./imgs/cards/5H.png"), cap: "5 of hearts" },
+      { img: require('./imgs/cards/3D.png'), cap: "3 of dimaonds" }
+   ];
 
-    const tileConfig = {
-        width: "250px",
-        height: "400px",
-        shadowColor: "rgba(150, 0, 0, 0.08)",
-        maxShadowBlur: 120,
-        traction: 17,
-        scale: 1.1
-    }
+   const slideConfig = {
+      containerWidth: "350px",
+      containerHeight: "500px",
+      imgWidth: "250px",
+      imgHeight: "400px",
+      shadowColor: "rgba(0,0,0,0.09)",
+      allowDots: true
+   }
 
-    const stackConfig = {
-        width: "250px",
-        height: "400px",
-        dotBgColor: "#888",
-        dotHighlightColor: "violet",
-        shadowColor: "rgba(100, 100, 100, 0.05)",
-        allowDots: false
-    }
+   const tileConfig = {
+      width: "250px",
+      height: "400px",
+      shadowColor: "rgba(150, 0, 0, 0.08)",
+      maxShadowBlur: 120,
+      traction: 17,
+      scale: 1.1
+   }
 
-    const importAll = (r) => {
-        console.log(r)
-        return r.keys().map(r);
-    };
+   const stackConfig = {
+      width: "250px",
+      height: "400px",
+      dotBgColor: "#888",
+      dotHighlightColor: "violet",
+      shadowColor: "rgba(100, 100, 100, 0.05)",
+      allowDots: false
+   }
 
-    //if you get TS context error-- - npm install - D @types/webpack-env
-    let images = importAll(
-        require.context("./imgs/cards", true, /\.(png|jpe?g|svg)$/)
-    );
+   const importAll = (r) => {
+      console.log(r)
+      return r.keys().map(r);
+   };
+
+   //if you get TS context error-- - npm install - D @types/webpack-env
+   let images = importAll(
+      require.context("./imgs/cards", true, /\.(png|jpe?g|svg)$/)
+   );
 
 
-    const aniSlidesConfig = {
-        containerWidth: "100%",
-        containerHeight: "1000px",
-        imgWidth: "960px",
-        imgHeight: "540px",
-        dotBgColor: "slategray",
-        dotHighlightColor: "slateblue"
-    }
+   const aniSlidesConfig = {
+      containerWidth: "100%",
+      containerHeight: "1000px",
+      imgWidth: "2000ppx",
+      imgHeight: "1250px",
+      dotBgColor: "slategray",
+      dotHighlightColor: "slateblue"
+   }
 
-    const aniImages = [
-        { img: require('./imgs/aniSlides/image1.jpg'), cap: "", key: 0 },
-        { img: require('./imgs/aniSlides/image2.jpg'), cap: "", key: 1 },
-        { img: require('./imgs/aniSlides/image3.jpg'), cap: "", key: 2 },
-        { img: require('./imgs/aniSlides/image4.jpg'), cap: "", key: 3 },
-        { img: require('./imgs/aniSlides/image5.jpg'), cap: "", key: 4 }
-    ]
+   const aniImages = [
+      { img: require('./imgs/aniSlides/image1.jpg'), cap: "" },
+      { img: require('./imgs/aniSlides/image2.jpg'), cap: "" },
+      { img: require('./imgs/aniSlides/image3.jpg'), cap: "" },
+      { img: require('./imgs/aniSlides/image4.jpg'), cap: "" },
+      { img: require('./imgs/aniSlides/image5.jpg'), cap: "" }
+   ]
 
-    return (
-        <div style={{ transform: "rotate(0.02deg)" }}>
+   return (
+      <div style={{ transform: "rotate(0.02deg)" }}>
 
-            <div className="app">
-                <Stack
-                    images={images}
-                    maxImgWidth="470px"
-                    maxImgHeight="600px"
-                    userConfig={stackConfig}
-                />
-            </div>
+         <div className="app">
+            <Stack
+               images={images}
+               maxImgWidth="470px"
+               maxImgHeight="600px"
+               userConfig={stackConfig}
+            />
+         </div>
 
-            <div className="tile_container">
+         <div className="tile_container">
 
-                <Tile config={tileConfig}>
-                    <img src={card} />
-                </Tile>
+            <Tile config={tileConfig}>
+               <img src={card} />
+            </Tile>
 
-            </div>
-            <div className="slide_container">
+         </div>
 
-                <SimpleSlides images={slideArr} userConfig={slideConfig} />
+         <div className="slide_container">
+            <SimpleSlides images={slideArr} userConfig={slideConfig} />
+         </div>
 
-            </div>
-
-            <div className="animated_slide_container">
-                <AnimatedSlides images={aniImages} userConfig={aniSlidesConfig} />
-            </div>
-        </div>
-    )
+         <div className="animated_slide_container">
+            <AnimatedSlides images={aniImages} userConfig={aniSlidesConfig} />
+         </div>
+      </div>
+   )
 }
 
 ReactDOM.render(<App />, document.getElementById('root'));
